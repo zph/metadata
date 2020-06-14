57812aa816ef2869195fb10c23e2f442c33ebeb9
Zander Hill <zander@xargs.io>
2020-06-14 09:45:40 -0700

    Version v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Metadata.MixProject do
   def project do
     [
       app: :metadata,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
